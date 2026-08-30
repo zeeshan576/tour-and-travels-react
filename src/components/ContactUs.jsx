2a00b411171a95db5462f3fc2ae20215e940dc71
zeeshan576 <shanhadi513@gmail.com>
2026-08-30 20:55:09 +0500

    Add contact form success message

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,23 +1,40 @@
+import { useState } from 'react'
 import './ContactUs.css'
 
-function ContactUs(){
+function ContactUs() {
+    const [submitted, setSubmitted] = useState(false)
+
     const handleSubmit = (e) => {
-        e.preventDefault();
-        e.target.reset();
+        e.preventDefault()
+
+        e.target.reset()
+        setSubmitted(true)
+
+        setTimeout(() => {
+            setSubmitted(false)
+        }, 3000)
     }
-    return(
+
+    return (
         <div className='form-container'>
             <h1>Send a message to us!</h1>
+
             <form onSubmit={handleSubmit}>
-                <input placeholder='Name'/>
-                <input placeholder='Email'/>
-                <input placeholder='Subject'/>
-                <textarea placeholder='Message' rows={4} ></textarea>
+                <input placeholder='Name' />
+                <input placeholder='Email' />
+                <input placeholder='Subject' />
+                <textarea placeholder='Message' rows={4}></textarea>
+
                 <button type='submit'>Send Message</button>
             </form>
-        </div>
 
+            {submitted && (
+                <p className='success-message'>
+                    Message submitted successfully!
+                </p>
+            )}
+        </div>
     )
 }
 
-export default ContactUs;
+export default ContactUs
